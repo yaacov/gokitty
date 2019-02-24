9c7ac614d89440565138dfb6ba3714a29421261b
yaacov <kobi.zamir@gmail.com>
2019-02-24 19:53:07 +0200

    move all tests to one file

--- pkg/mux/router_test.go
+++ pkg/mux/router_test.go
@@ -18,6 +18,8 @@ package mux
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -33,6 +35,93 @@ func found(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(200)
 	io.WriteString(w, fmt.Sprintf("{\"key\": \"%s\"}", value))
+}
+
+func benchmarkHandler(w http.ResponseWriter, r *http.Request) {
+	value, _ := Var(r, "key")
+
+	w.WriteHeader(200)
+	io.WriteString(w, fmt.Sprintf("{\"key\": \"%s\"}", value))
+}
+
+// ExampleRouter tests a route.
+func ExampleRouter() {
+	// A cat handler for the kitty request.
+	catHandler := func(w http.ResponseWriter, r *http.Request) {
+		// Retrieve the ":uid" route parameter.
+		uid, _ := Var(r, "uid")
+
+		io.WriteString(w, fmt.Sprintf("{\"uid\": \"%s\", \"name\": \"Layla\"}", uid))
+	}
+
+	// Create a new router and egister our routes.
+	router := Router{
+		// Optional custom "404 - page not found" handler.
+		// If not defined, router will fallback to a default 404.4 handler.
+		//NotFoundHandler: notFoundHandler,
+	}
+	// Routes can have optional route parameters, in this example
+	// route, ":uid" is a route parameter, once a route is dispatched,
+	// the value of ":uid" can be retrieved using the
+	// `mux.Var(*http.Request, string)` method.
+	//
+	// Example:
+	// For a request "http://localhost:8080/kitty/eyfgt654efg7198u",
+	// the value of ":uid" route parameter will be "eyfgt654efg7198u"
+	router.HandleFunc("GET", "/kitty/:uid", catHandler)
+
+	// Start the http server.
+	ts := httptest.NewServer(router)
+	defer ts.Close()
+
+	// Query server.
+	res, err := http.Get(ts.URL + "/kitty/eyfgt654efg7198u")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	catJason, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s", catJason)
+
+	// Output: {"uid": "eyfgt654efg7198u", "name": "Layla"}
+}
+
+// BenchmarkRouter benchmarks routing handler.
+func BenchmarkRouter(b *testing.B) {
+	// Create a router with 3 posible routes.
+	router := Router{}
+	router.HandleFunc("GET", "/found", benchmarkHandler)
+	router.HandleFunc("GET", "/found/:key", benchmarkHandler)
+	router.HandleFunc("GET", "/found/:key/info", benchmarkHandler)
+
+	// Handle a request with on route parameter N times.
+	for n := 0; n < b.N; n++ {
+		req, err := http.NewRequest("GET", "/found/hello", nil)
+		if err != nil {
+			b.Fatal(err)
+		}
+
+		rr := httptest.NewRecorder()
+		router.ServeHTTP(rr, req)
+
+		// Check the status code is what we expect.
+		if status := rr.Code; status != http.StatusOK {
+			b.Errorf("handler returned wrong status code: got %v want %v",
+				status, http.StatusOK)
+		}
+
+		// Check the response body is what we expect.
+		expected := `{"key": "hello"}`
+		if rr.Body.String() != expected {
+			b.Errorf("handler returned unexpected body: got %v want %v",
+				rr.Body.String(), expected)
+		}
+	}
 }
 
 func TestDefaultNotFound(t *testing.T) {
